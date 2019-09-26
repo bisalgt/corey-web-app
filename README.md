2138f15a37df14172609e18b8a85d8fc40c2481c
Bishal <bisalgt@gmail.com>
2019-09-26 21:09:23 +0545

    working with models and database

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # corey-web-app
 Making a blog website.
+Creating superuser only after making database table, by migration
+python manage.py makemigrations -- detects the changes and makes django ready for update the database
+to apply changes use-- python manage.py migrate
+creating database in models
+each class of models.py is going to be tables in database
+each attributes is going to be field of that database
+To view the sql code-- python manage.py sqlmigrate appname migration_number
+
+to work with the database in shell
+python manage.py shell
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-# from django.http import HttpResponse 
+from .models import Post
 
-
-posts = [
-    {
-        'author':'Bishal Gautam',
-        'title':'Blog Post 1',
-        'content':'First Post Content',
-        'date_posted':'August 28, 2018'
-    },
-    {
-        'author':'Jane Doe',
-        'title':'Blog Post 2',
-        'content':'Second Post Content',
-        'date_posted':'August 29, 2018'
-    },
-
-]
 
 
 def home(request): # gets request from url as first argument// we can name any
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
